1d56ae8d8e053fd05c4fb2682f314adb6e09e05c
Adolfo <you@example.com>
2018-10-25 18:47:31 -0700

    added other files

---
+++ lab6prework/balloc.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ext2fs/ext2_fs.h>
+
+// define shorter TYPES, save typing efforts
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_super_block SUPER;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;    // need this for new version of e2fs
+
+#define BLKSIZE 1024
+
+GD    *gp;
+SUPER *sp;
+INODE *ip;
+DIR   *dp; 
+
+/********** globals *************/
+int fd;
+int imap, bmap;  // IMAP and BMAP block number
+int ninodes, nblocks, nfreeInodes, nfreeBlocks;
+
+int get_block(int fd, int blk, char buf[ ])
+{
+  lseek(fd, (long)blk*BLKSIZE, 0);
+  read(fd, buf, BLKSIZE);
+}
+
+int put_block(int fd, int blk, char buf[ ])
+{
+  lseek(fd, (long)blk*BLKSIZE, 0);
+  write(fd, buf, BLKSIZE);
+}
+
+int tst_bit(char *buf, int bit)
+{
+  int i, j;
+  i = bit/8; j=bit%8;
+  if (buf[i] & (1 << j))
+     return 1;
+  return 0;
+}
+
+int set_bit(char *buf, int bit)
+{
+  int i, j;
+  i = bit/8; j=bit%8;
+  buf[i] |= (1 << j);
+}
+
+int clr_bit(char *buf, int bit)
+{
+  int i, j;
+  i = bit/8; j=bit%8;
+  buf[i] &= ~(1 << j);
+}
+
+int decFreeInodes(int dev)
+{
+  char buf[BLKSIZE];
+
+  // dec free inodes count in SUPER and GD
+  get_block(dev, 1, buf);
+  sp = (SUPER *)buf;
+  sp->s_free_inodes_count--;
+  put_block(dev, 1, buf);
+
+  get_block(dev, 2, buf);
+  gp = (GD *)buf;
+  gp->bg_free_inodes_count--;
+  put_block(dev, 2, buf);
+}
+
+int balloc(int dev)
+{
+  int  i;
+  char buf[BLKSIZE];
+
+  // read inode_bitmap block
+  get_block(dev, bmap, buf);
+
+  for (i=0; i < ninodes; i++){
+    if (tst_bit(buf, i)==0){
+       set_bit(buf,i);
+       decFreeInodes(dev);
+
+       put_block(dev,  bmap, buf);
+
+       return i+1;
+    }
+  }
+  printf("balloc(): no more free blocks\n");
+  return 0;
+}
+
+char *disk = "mydisk";
+
+int main(int argc, char *argv[ ])
+{
+  int i, block_no;
+  char buf[BLKSIZE];
+
+  if (argc > 1)
+    disk = argv[1];
+
+  fd = open(disk, O_RDWR);
+  if (fd < 0){
+    printf("open %s failed\n", disk);
+    exit(1);
+  }
+
+  // read SUPER block
+  get_block(fd, 1, buf);
+  sp = (SUPER *)buf;
+
+  ninodes = sp->s_inodes_count;
+  nblocks = sp->s_blocks_count;
+  nfreeInodes = sp->s_free_inodes_count;
+  nfreeBlocks = sp->s_free_blocks_count;
+  printf("ninodes=%d nblocks=%d nfreeInodes=%d nfreeBlocks=%d\n", 
+	 ninodes, nblocks, nfreeInodes, nfreeBlocks);
+
+  // read Group Descriptor 0
+  get_block(fd, 2, buf);
+  gp = (GD *)buf;
+
+  bmap = gp->bg_block_bitmap;
+  printf("bmap = %d\n", bmap);
+  getchar();
+
+  for (i=0; i < 5; i++){  
+    block_no = balloc(fd);
+    printf("allocated ino = %d\n", block_no);
+  }
+}
